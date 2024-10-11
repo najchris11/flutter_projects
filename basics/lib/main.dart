d39164dec3a43b0b80927eef198746c3fd9e55da
Najib <52837430+najchris11@users.noreply.github.com>
2024-10-10 21:32:52 -0400

    let's go lesson 30

---
+++ basics/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!', style: TextStyle(color: Colors.white),), //wooo dark modeeeef= heeh
+        ),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  );
+}
