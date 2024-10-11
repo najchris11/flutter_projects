cb8648373213e7618c6c84c5de2e13adfc268b87
Najib <52837430+najchris11@users.noreply.github.com>
2024-10-10 22:03:14 -0400

    Break for today. Learned a bit about the general structure of dart. It's basically Java lol. I wonder how many languages I'll learn that are essentially Java++.

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!', style: TextStyle(color: Colors.white),), //wooo dark modeeeef= heeh
-        ),
+        body: GradientContainer(),
         backgroundColor: Colors.black,
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget{
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.grey, Colors.blueGrey, Colors.lightBlue],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomCenter),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28), //proud of myself for using my brain to figure out how to do it before he told us to
+            ), //wooo dark modeeeef= heeh
+          ),
+        );
+  }
 }
